80c57f88f293f1def10dbb2d55c36c7bcbe259a2
Artyom Mordvinov <1992bress@gmail.com>
2020-12-08 10:26:49 +0300

    avoid check

--- src/UserManager.js
+++ src/UserManager.js
@@ -228,10 +228,6 @@ export class UserManager extends OidcClient {
                         Log.error("UserManager._validateIdTokenFromTokenRefreshToken: sub in id_token does not match current sub");
                         return Promise.reject(new Error("sub in id_token does not match current sub"));
                     }
-                    if (payload.auth_time && payload.auth_time !== profile.auth_time) {
-                        Log.error("UserManager._validateIdTokenFromTokenRefreshToken: auth_time in id_token does not match original auth_time");
-                        return Promise.reject(new Error("auth_time in id_token does not match original auth_time"));
-                    }
                     if (payload.azp && payload.azp !== profile.azp) {
                         Log.error("UserManager._validateIdTokenFromTokenRefreshToken: azp in id_token does not match original azp");
                         return Promise.reject(new Error("azp in id_token does not match original azp"));
